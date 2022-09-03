205bb29b31efe25bc50100e11bebabda4aaac13d
luyizhen <luyizhen93@gmail.com>
2022-09-03 10:31:51 +0800

    Fix: bg color
    1. Change bg color. 2. Refactor app. 3. Add variable scss.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import Box from "@mui/material/Box";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "../components/common/commonMuiTheme";
 
@@ -10,11 +11,15 @@ import style from "../sass/layout.module.scss";
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <div className={style.app}>
+      <Box
+        sx={{ width: "100%", height: "100%" }}
+        // bgcolor={`rgba(${theme.palette.primary.light}, 0.3)`}
+        className={style.app}
+      >
         <Header />
         <Outlet />
         <Footer />
-      </div>
+      </Box>
     </ThemeProvider>
   );
 };
