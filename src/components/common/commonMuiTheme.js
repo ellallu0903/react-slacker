de6ce8fe91ab73844234c6e880173a98a959c8c3
luyizhen <luyizhen93@gmail.com>
2022-09-03 10:12:05 +0800

    Fix: header RWD & remove useless scss

--- src/components/common/commonMuiTheme.js
+++ src/components/common/commonMuiTheme.js
@@ -9,4 +9,12 @@ export const theme = createTheme({
       contrastText: "#fff",
     },
   },
+  typography: {
+    h5: {
+      fontSize: "1.2rem",
+      "@media (min-width:600px)": {
+        fontSize: "1.5rem",
+      },
+    },
+  },
 });
